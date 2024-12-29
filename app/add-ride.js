c1b99f7c110a2d13032c06ef1df3386ce5badb86
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2024-12-29 16:00:43 +0530

    create add-ride page

---
+++ app/add-ride.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import MapView from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
+const AddRide = () => {
+  return (
+    <View>
+    <Text>
+        Add Your Ride Here
+    </Text>
+    
+  </View>
+  )
+}
+export default AddRide;

c1f9cb902744f1547c87564095ea8eefa7e4dd0f
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2024-12-29 22:12:38 +0530

    create introtwo.js file

--- app/introtwo.js
+++ app/introtwo.js
@@ -1,16 +1,15 @@
-
 import React from 'react'
 import { Pressable, Text, View } from 'react-native'
 import { Link, router } from "expo-router";
 
-const introone = () => {
+const introtwo = () => {
     
   return (
     <View className="max-w-screen">
-      <Text className="mt-5 text-4xl text-center ">Welcome to ShareMyRide
-      {"\n"}-Your Journey Your Way</Text>
+      <Text className="mt-5 text-4xl text-center ">Choose Your Ride
+      {"\n"}-Join With Others Ride</Text>
       <Text className="mt-5 text-center">
-        "Welcome to ShareMyRide – Your Journey,{"\n"} Shared. Discover convenient, {"\n"}affordable rides with others heading your way. {"\n"}Let’s get you moving!"
+        "Find Your Perfect Ride. {"\n"}Browse available routes and book your spot in just a few taps. {"\n"}Travel with ease and meet fellow travelers."
       </Text>
       <View className="flex items-center justify-center flex-row gap-10">
 
@@ -20,7 +19,7 @@ const introone = () => {
             <Text className="text-black">Skip</Text>
         </Pressable>
 
-        <Pressable  onPress={()=>{router.push("/introtwo")}} 
+        <Pressable  onPress={()=>{router.push("/introthird")}} 
          className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <Text className="text-white" >Next</Text>
           </Pressable>
@@ -30,4 +29,4 @@ const introone = () => {
   );
 }
 
-export default introone
+export default introtwo;

36ec7f5cf8995dc1813c91d938c869473de4cd6f
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2024-12-31 17:30:37 +0530

    setting up into page paths

--- app/introtwo.js
+++ app/introtwo.js
@@ -19,7 +19,7 @@ const introtwo = () => {
             <Text className="text-black">Skip</Text>
         </Pressable>
 
-        <Pressable  onPress={()=>{router.push("/")}} 
+        <Pressable  onPress={()=>{router.push("/introthird")}} 
          className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <Text className="text-white" >Next</Text>
           </Pressable>

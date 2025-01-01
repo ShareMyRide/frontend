7d6e1afcae1e4d73fcbcd2a0899370f6d36fdcfe
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2025-01-01 22:36:04 +0530

    create profile page
    linked from dashboard

--- app/dashboard.js
+++ app/dashboard.js
@@ -5,6 +5,12 @@ import { Link,router } from "expo-router";
 const dashboard = () => {
   return (
     <View className="max-w-screen">
+      <Pressable
+              onPress={() => {
+                router.push("/profile")}}
+                className="bg-white flex justify-items-end mt-4 text-white hover:bg-gray-500 font-bold py-1 px-4 rounded">
+                  <Text className="mt-5 text-sm text-right" >Profile</Text>
+            </Pressable>
       <Text className="mt-5 text-3xl text-center">dashboard</Text>
       <Text className="mt-5 text-center">
         Find your Ride or Share your Ride
@@ -19,6 +25,7 @@ const dashboard = () => {
         <Pressable className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <Text className="text-white">Find Ride</Text>
         </Pressable>
+
       </View>
     </View>
   );

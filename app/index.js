f327ff85da09d98d51db4d61aed6f259e1cd9af0
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2025-01-02 23:44:47 +0530

    fixed : global.css errors

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
-import { Pressable,Image, View, Text, ActivityIndicator, StyleSheet } from 'react-native';
-import { useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import {
+  Pressable,
+  Image,
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+} from "react-native";
+import { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 import { Link, router } from "expo-router";
 export default function App() {
   /*const navigation = useNavigation();
@@ -14,20 +21,28 @@ export default function App() {
   }, []); 
 */
   return (
-  <View onClick={()=> {navigation.navigate('/introone')}}
-    className="p-4 h-screen flex items-center justify-center bg-lime-500">
+    <View
+      onClick={() => {
+        navigation.navigate("/introone");
+      }}
+      className="p-4 h-screen flex items-center justify-center bg-lime-500"
+    >
       <Text className="mt-5 text-3xl font-bold text-white">welcome</Text>
       <Text className="mb-10 text-6xl font-">Share My Ride</Text>
-       <Image
-                  source={require("../assets/images/ShareMyRide-logo.jpg")}
-                  style={{ width: 150, height: 150 }}
-                />
-      <View /> 
-      <ActivityIndicator size="large" color="#343438" /> 
-      <Pressable  onPress={()=>{router.push("/introtwo")}} 
-               className="bg-black mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  <Text className="text-white" >Next</Text>
-                </Pressable>
+      <Image
+        source={require("../assets/images/ShareMyRide-logo.jpg")}
+        style={{ width: 150, height: 150 }}
+      />
+      <View />
+      <ActivityIndicator size="large" color="#343438" />
+      <Pressable
+        onPress={() => {
+          router.push("/introtwo");
+        }}
+        className="bg-black mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        <Text className="text-white">Next</Text>
+      </Pressable>
     </View>
   );
 }

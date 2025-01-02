d4c0c8579342727abdb8bd795e41c15ca84960b2
kaveeshabasnayaka <kaveeshabasnayaka9@gmail.com>
2025-01-02 14:14:04 +0530

    create map.js

---
+++ app/map.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import axios from 'axios';

23f51d688c2d4462962c07efeb4209b8bbb4e4db
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2025-01-02 20:39:57 +0530

    add list view to find ride and text search to it

--- app/find-ride.js
+++ app/find-ride.js
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; 
 
 const FindRide = () => {
   const [searchText, setSearchText] = useState('');
   const windowWidth = Dimensions.get('window').width; 
+
+  const rides = [
+    { 
+      start: 'Colombo', 
+      end: 'Kandy', 
+      seats: 2 
+    },
+    { 
+      start: 'Galle', 
+      end: 'Matara', 
+      seats: 1 
+    },
+    { 
+      start: 'Jaffna', 
+      end: 'Colombo', 
+      seats: 3 
+    },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.rideItem}>
+      <Text style={styles.rideLabel}>Starting Point:</Text>
+      <Text>{item.start}</Text> 
+      <Text style={styles.rideLabel}>Ending Point:</Text>
+      <Text>{item.end}</Text> 
+      <Text style={styles.rideLabel}>Available Seats:</Text>
+      <Text>{item.seats}</Text> 
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -21,6 +50,14 @@ const FindRide = () => {
           placeholderTextColor="lightgray"
           value={searchText}
           onChangeText={(text) => setSearchText(text)} 
+        />
+      </View>
+
+      <View style={styles.ridesList}> 
+        <FlatList 
+          data={rides}
+          renderItem={renderItem} 
+          keyExtractor={(item) => item.start + item.end} // Unique key for each item
         />
       </View>
     </View>
@@ -44,7 +81,20 @@ const styles = StyleSheet.create({
   },
   input: {
     flex: 1,
-    fontSize: 16, 
+    fontSize: 16,
+  },
+  ridesList: {
+    marginTop: 20, 
+  },
+  rideItem: {
+    borderWidth: 1, 
+    borderColor: '#ccc', 
+    borderRadius: 5, 
+    padding: 10, 
+    marginBottom: 10, 
+  },
+  rideLabel: {
+    fontWeight: 'bold', 
   },
 });
 

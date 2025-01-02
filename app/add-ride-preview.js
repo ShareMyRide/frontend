516ea298eea7a7e0590d129fa05fa9e84af10a85
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2025-01-02 14:00:25 +0530

    add: Rides to toggle menu

--- app/add-ride-preview.js
+++ app/add-ride-preview.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { FlatList,Text, View } from 'react-native';
-const addridepreview = () =>{
-    return(
-        <View className="mt-10 flex flex-col justify-between items-center " >
-            <Text className="font-bold text-3xl "> Your Rides </Text>
-            <FlatList className="mt-10"
-        data={[
-          {key: 'Ride date:'},
-          {key: 'Ride details: '},
-          {key: 'ride status:'}
-        ]}
-        renderItem={({item}) => <Text>{item.key}</Text>}
-      />
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+const addridepreview = () => {
+  return (
+    <View className="mt-10 flex flex-col justify-between items-center ">
+      <Text className="font-bold text-3xl "> Your Rides </Text>
+      
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Ride Details</th>
+            <th>Date</th>
+            <th>Active Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
             
-        </View>
-    );
-}
+          </tr>
+          <tr>
+            
+          </tr>
+          <tr>
+            
+          </tr>
+        </tbody>
+      </table>
+    </View>
+  );
+};
 export default addridepreview;
